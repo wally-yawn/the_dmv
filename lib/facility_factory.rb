debb9e60c0702b90b10c5467985696bc0bc0c575
Wally Wallace <jrvw001@gmail.com>
2024-09-03 16:06:46 -0600

    Add ability to create NY facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,13 +4,37 @@ class FacilityFactory
     @facilities = []
   end
 
-  def create_facilities(facilities)
-    
-    facilities.each do |facility|
+  def format_address(address_1, address_2, city, state, zip)
+    if address_2 != nil
+      "#{address_1} #{address_2} #{city} #{state} #{zip}"
+    else
+      "#{address_1} #{city} #{state} #{zip}"
+    end
+  end
+
+  def create_co_facilities(co_facilities)
+    co_facilities.each do |facility|
       name = facility[:dmv_office]
-      address = "#{facility[:address_li]} #{facility[:address__1]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}"
+      address = format_address(facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip])
       phone = facility[:phone]
       @facilities << Facility.new({name: name, address: address, phone: phone})
+    end
+  end
+
+  def create_ny_facilities(ny_facilities)
+    ny_facilities.each do |facility|
+      name = facility[:office_name]
+      address = format_address(facility[:street_address_line_1], facility[:street_address_line_2], facility[:city], facility[:state], facility[:zip_code])
+      phone = facility[:public_phone_number]
+      @facilities << Facility.new({name: name, address: address, phone: phone})
+    end
+  end
+
+  def create_facilities(facilities)
+    if facilities[0][:state] == 'CO'
+      create_co_facilities(facilities)
+    elsif facilities[0][:state] == 'NY'
+      create_ny_facilities(facilities)
     end
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,11 +4,43 @@ RSpec.describe FacilityFactory do
   before(:each) do
     @facility_factory1 = FacilityFactory.new
     @co_locations = DmvDataService.new.co_dmv_office_locations
+    @ny_locations = DmvDataService.new.ny_dmv_office_locations
   end
 
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility_factory1).to be_an_instance_of(FacilityFactory)
+    end
+  end
+
+  describe '#create_co_facilities' do
+    it 'can create co facilities_directly' do
+      expect(@facility_factory1.facilities).to eq([])
+      @facility_factory1.create_co_facilities(@co_locations)
+      expect(@facility_factory1.facilities[0]).to be_an_instance_of(Facility)
+      expect(@facility_factory1.facilities[0].name).to eq('DMV Tremont Branch')
+      expect(@facility_factory1.facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+      expect(@facility_factory1.facilities[0].phone).to eq('(720) 865-4600')
+      expect(@facility_factory1.facilities[0].services).to eq([])
+      expect(@facility_factory1.facilities[0].registered_vehicles).to eq([])
+      expect(@facility_factory1.facilities[0].collected_fees).to eq(0)
+      expect(@facility_factory1.facilities.length).to eq(5)
+    end
+  end
+
+
+  describe '#create_ny_facilities' do
+    it 'can create ny facilities_directly' do
+      expect(@facility_factory1.facilities).to eq([])
+      @facility_factory1.create_ny_facilities(@ny_locations)
+      expect(@facility_factory1.facilities[0]).to be_an_instance_of(Facility)
+      expect(@facility_factory1.facilities[0].name).to eq('HUDSON')
+      expect(@facility_factory1.facilities[0].address).to eq('560 WARREN STREET HUDSON NY 12534')
+      expect(@facility_factory1.facilities[0].phone).to eq('5188283350')
+      expect(@facility_factory1.facilities[0].services).to eq([])
+      expect(@facility_factory1.facilities[0].registered_vehicles).to eq([])
+      expect(@facility_factory1.facilities[0].collected_fees).to eq(0)
+      expect(@facility_factory1.facilities.length).to eq(172)
     end
   end
 
@@ -25,5 +57,20 @@ RSpec.describe FacilityFactory do
       expect(@facility_factory1.facilities[0].collected_fees).to eq(0)
       expect(@facility_factory1.facilities.length).to eq(5)
     end
+
+    it 'can create ny facilities' do
+      expect(@facility_factory1.facilities).to eq([])
+      @facility_factory1.create_facilities(@ny_locations)
+      expect(@facility_factory1.facilities[1]).to be_an_instance_of(Facility)
+      expect(@facility_factory1.facilities[1].name).to eq('ROCHESTER DOWNTOWN')
+      expect(@facility_factory1.facilities[1].address).to eq('200 E. MAIN STREET STE. 101 ROCHESTER NY 14604')
+      expect(@facility_factory1.facilities[1].phone).to eq('5852594526')
+      expect(@facility_factory1.facilities[1].services).to eq([])
+      expect(@facility_factory1.facilities[1].registered_vehicles).to eq([])
+      expect(@facility_factory1.facilities[1].collected_fees).to eq(0)
+      expect(@facility_factory1.facilities.length).to eq(172)
+    end
   end
+
+  #format address tests
 end
